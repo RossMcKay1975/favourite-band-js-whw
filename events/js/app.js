811bbf013f6ea4c906465fafae761876f3aedb59
Ross Mckay <ross.mckay1975@gmail.com>
2019-09-07 10:42:27 +0100

    mvp

---
+++ events/js/app.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOMContentLoaded', () => {
+const newItemForm = document.querySelector('#new-item-form');
+newItemForm.addEventListener('submit', handleNewItemFormSubmit);
+
+const deleteAllButton = document.querySelector('#delete-all');
+deleteAllButton.addEventListener('click', handleDeleteAllClick);
+})
+const handleNewItemFormSubmit = function (event) {
+  event.preventDefault();
+
+  const favouriteAlbumListItem = createFavouriteAlbumListItem(event.target);
+  const favouriteAlbumList = document.querySelector('#favourite-album-list');
+  favouriteAlbumList.appendChild(favouriteAlbumListItem);
+
+  event.target.reset()
+}
+
+const createFavouriteAlbumListItem = function (form) {
+  const favouriteAlbumListItem = document.createElement('li');
+  favouriteAlbumListItem.classList.add('favourite-album-list-item');
+
+  const bandArtist = document.createElement('h2');
+  bandArtist.textContent = form.bandArtist.value;
+  favouriteAlbumListItem.appendChild(bandArtist);
+
+  const album = document.createElement('h1');
+  album.textContent = form.album.value;
+  console.dir(event)
+  favouriteAlbumListItem.appendChild(album);
+
+  const category = document.createElement('p');
+  category.textContent = form.category.value;
+  favouriteAlbumListItem.appendChild(category);
+
+  return favouriteAlbumListItem;
+}
+
+  const handleDeleteAllClick = function (event) {
+    const favouriteAlbumList = document.querySelector('#favourite-album-list');
+    favouriteAlbumList.innerHTML = '';
+  }
